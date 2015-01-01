4d1dd25d814a2b7daee9dcbc7605276cd2b27e88
Luke Derrick <lderrickable@gmail.com>
2014-12-31 19:45:59 -0500

    Made gulpfile.js with SIMPLE_OPTIMIZATIONS.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ gulp.task('default', function() {
   gulp.src('index.js')
     .pipe(closureCompiler({
       compilerPath: 'bower_components/closure-compiler/lib/vendor/compiler.jar',
-      fileName: 'build.js'
+      fileName: 'build.js',
+      compilerFlags: {
+      	compilation_level:'SIMPLE_OPTIMIZATIONS'
+      }
     }))
     .pipe(gulp.dest('dista'));
 });
